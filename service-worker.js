69febe9f96716960e273b14ba2d0083723edbe39
zabogptplus-hash <zabogptplus@gmail.com>
2025-09-25 22:34:01 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+
+const CACHE='legacy-planner-final-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./service-worker.js'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))) });
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
