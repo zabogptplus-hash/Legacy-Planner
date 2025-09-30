7f610742c9ebf9f8de39a14a26171eb306f79729
zabogptplus-hash <zabogptplus@gmail.com>
2025-09-30 15:08:50 +0800

    Add files via upload
    Signed-off-by: zabogptplus-hash <zabogptplus@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,25 @@
+// Basic cache-first service worker
+const CACHE = 'legacy-planner-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
+];
 
-const CACHE='legacy-planner-final-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./service-worker.js'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))) });
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k!==CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  }
+});
